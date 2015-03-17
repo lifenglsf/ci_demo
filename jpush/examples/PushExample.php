9ccb5a58526e54e5d2ee5acd644fb18272b33a3d
lifeng <lifenglsf@163.com>
2015-03-17 11:07:06 +0800

    'jpush'

--- jpush/examples/PushExample.php
+++ jpush/examples/PushExample.php
@@ -19,35 +19,34 @@ JPushLog::setLogHandlers(array(new StreamHandler('jpush.log', Logger::DEBUG)));
 $client = new JPushClient($app_key, $master_secret);
 
 //easy push
-try {
-	$result = $client->push()
-	                 ->setPlatform(M\all)
-	                 ->setAudience(M\all)
-	                 ->setNotification(M\notification('Hi, JPush'))
-	                 ->printJSON()
-	                 ->send();
-	echo 'Push Success.' . $br;
-	echo 'sendno : ' . $result->sendno . $br;
-	echo 'msg_id : ' . $result->msg_id . $br;
-	echo 'Response JSON : ' . $result->json . $br;
+/*try {
+$result = $client->push()
+->setPlatform(M\all)
+->setAudience(M\all)
+->setNotification(M\notification('Hi, JPush'))
+->printJSON()
+->send();
+echo 'Push Success.' . $br;
+echo 'sendno : ' . $result->sendno . $br;
+echo 'msg_id : ' . $result->msg_id . $br;
+echo 'Response JSON : ' . $result->json . $br;
 } catch (APIRequestException $e) {
-	echo 'Push Fail.' . $br;
-	echo 'Http Code : ' . $e->httpCode . $br;
-	echo 'code : ' . $e->code . $br;
-	echo 'Error Message : ' . $e->message . $br;
-	echo 'Response JSON : ' . $e->json . $br;
-	echo 'rateLimitLimit : ' . $e->rateLimitLimit . $br;
-	echo 'rateLimitRemaining : ' . $e->rateLimitRemaining . $br;
-	echo 'rateLimitReset : ' . $e->rateLimitReset . $br;
+echo 'Push Fail.' . $br;
+echo 'Http Code : ' . $e->httpCode . $br;
+echo 'code : ' . $e->code . $br;
+echo 'Error Message : ' . $e->message . $br;
+echo 'Response JSON : ' . $e->json . $br;
+echo 'rateLimitLimit : ' . $e->rateLimitLimit . $br;
+echo 'rateLimitRemaining : ' . $e->rateLimitRemaining . $br;
+echo 'rateLimitReset : ' . $e->rateLimitReset . $br;
 } catch (APIConnectionException $e) {
-	echo 'Push Fail: ' . $br;
-	echo 'Error Message: ' . $e->getMessage() . $br;
-	//response timeout means your request has probably be received by JPUsh Server,please check that whether need to be pushed again.
-	echo 'IsResponseTimeout: ' . $e->isResponseTimeout . $br;
+echo 'Push Fail: ' . $br;
+echo 'Error Message: ' . $e->getMessage() . $br;
+//response timeout means your request has probably be received by JPUsh Server,please check that whether need to be pushed again.
+echo 'IsResponseTimeout: ' . $e->isResponseTimeout . $br;
 }
 
-echo $br . '-------------' . $br;
-exit;
+echo $br . '-------------' . $br;*/
 
 // easy push with ios badge +1
 // 以下演示推送给 Android, IOS 平台下Tag为tag1的用户的示例
@@ -83,6 +82,7 @@ try {
 }
 
 echo $br . '-------------' . $br;
+exit;
 
 //full push
 try {
